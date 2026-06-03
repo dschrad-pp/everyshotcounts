f52b4ea38530c4e709e4268937c128eb08855f4f
rdangol25 <rdangol25@my.whitworth.edu>
2026-06-03 00:45:30 -0700

    -Fixed issue

--- pallbearer-core/src/main/java/com/lektralabs/thrones/pallbearer/jdbi/service/ContactService.java
+++ pallbearer-core/src/main/java/com/lektralabs/thrones/pallbearer/jdbi/service/ContactService.java
@@ -1,6 +1,5 @@
 package com.lektralabs.thrones.pallbearer.jdbi.service;
 
-import com.lektralabs.thrones.pallbearer.jdbi.dao.ContactDao;
 import com.lektralabs.thrones.pallbearer.jdbi.service.generated.ContactBaseService;
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -12,12 +11,9 @@ import org.jboss.logging.Logger;
 public class ContactService extends ContactBaseService {
     private static final Logger logger = Logger.getLogger(ContactService.class);
 
-    private ContactDao contactDao;
-
     @PostConstruct
     public void init() {
         super.init();
-        this.contactDao = jdbiProvider.getJdbi().onDemand(ContactDao.class);
     }
 
 
